af16347e7091594fe9a437a9bbc424aa9ad8bee0
Pieter <pieterbrandsen2@gmail.com>
2021-06-28 16:08:23 +0200

    Removed Debug files

---
+++ OnlineMarketingTools.Core/Models/Seed.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarketingTools.Core.Models
+{
+    public class UserSeed
+    {
+        public int Key { get;set;  }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Adress { get; set; }
+        public int HouseNumber { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+    }
+    public class MedicalSeed : UserSeed
+    {
+
+    }
+    public class ProductSeed : UserSeed
+    {
+
+    }
+    public class HobbySeed : UserSeed
+    {
+
+    }
+}
