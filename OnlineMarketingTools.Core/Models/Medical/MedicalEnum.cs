97f481376b4830c52331e42904c29b38e895ccef
Rowan Brouwer <Rowan.Brouwer@itvitaelearning.nl>
2021-06-29 11:04:20 +0200

    expanded hobby-, medical- and product- person classes with enum.

---
+++ OnlineMarketingTools.Core/Models/Medical/MedicalEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMarketingTools.Core.Models.Medical
+{
+    public enum MedicalEnum
+    {
+        Injured,
+        Uninjured
+    }
+}
