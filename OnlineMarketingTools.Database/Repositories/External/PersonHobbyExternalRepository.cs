1cb4238905a35ea61a7c8427882f87f288dcefcf
Rowan Brouwer <Rowan.Brouwer@itvitaelearning.nl>
2021-07-01 13:22:25 +0200

    Completed ExtHobbyRepo.

--- OnlineMarketingTools.Database/Repositories/External/PersonHobbyExternalRepository.cs
+++ OnlineMarketingTools.Database/Repositories/External/PersonHobbyExternalRepository.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using OnlineMarketingTools.Core.Interfaces;
 using OnlineMarketingTools.DataExternal.Data;
 using OnlineMarketingTools.DataExternal.Entities;
@@ -9,10 +12,10 @@ namespace OnlineMarketingTools.Database.Repositories.External
 {
 	public class PersonHobbyExternalRepository : IExternalRepository<PersonHobby>
     {
-        private readonly PersonHobbyDbContext context;
+        private readonly PersonHobbyDbContext _context;
         public PersonHobbyExternalRepository(PersonHobbyDbContext context)
         {
-            this.context = context;
+            this._context = context;
         }
 
         //Todo
@@ -21,19 +24,24 @@ namespace OnlineMarketingTools.Database.Repositories.External
         /// Gets an IEnumerable<PersonHobby> of All entity's in this DB
         /// </summary>
         /// <returns>Task<IEnumerable<PersonHobby>></returns>
-        public Task<IEnumerable<PersonHobby>> GetAll()
-        {
-            throw new NotImplementedException();
+        public async Task<IEnumerable<PersonHobby>> GetAll()
+{
+            return await _context.PersonHobbies.ToListAsync();
         }
 
         /// <summary>
-        /// Gets a list of people by fieldname
+        /// Gets a IEnumerable<PersonHobby> based on the name of the field and the value you want that field to have.
         /// </summary>
-        /// <param name="fieldName"> The name of the field you want to search for </param>
-        /// <returns>Task<IEnumerable<PersonHobby>></returns>
-        public Task<IEnumerable<PersonHobby>> GetAllByFieldName(string value, string fieldName)
+        /// <param name="value">The value of the field you want</param>
+        /// <param name="fieldName">The name of the field you want to check</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<PersonHobby>> GetAllByFieldName(string value, string fieldName)
         {
-            throw new NotImplementedException();
+            var result = _context.PersonHobbies
+               .Where(string.Format("{0} == {1}", fieldName, value))
+               .AsEnumerable<PersonHobby>();
+
+            return await Task.FromResult(result);
         }
 
     }
