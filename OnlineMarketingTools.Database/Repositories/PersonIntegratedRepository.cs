263dee44f7c3144530552eece2f09713e39b1c4e
Jonathan Meibergen <jonathanmeibergen@icloud.com>
2021-07-05 13:44:26 +0200

    changed namespace for PersonIntegratedRepository

--- OnlineMarketingTools.Database/Repositories/PersonIntegratedRepository.cs
+++ OnlineMarketingTools.Database/Repositories/PersonIntegratedRepository.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using OnlineMarketingTools.Core.Entities;
 using OnlineMarketingTools.Core.Interfaces;
 using OnlineMarketingTools.DataExternal.Data;
-using OnlineMarketingTools.DataExternal.Entities;
 
-namespace OnlineMarketingTools.DataExternal.Repositories
+namespace OnlineMarketingTools.Database.Repositories
 {
 	public class PersonIntegratedRepositoy : IPersonIntegratedRepository
     {
