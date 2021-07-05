b82d1e34a4c200df35b8a2d0d1c9117f46e04666
Jonathan Meibergen <jonathanmeibergen@icloud.com>
2021-07-05 15:05:34 +0200

    template OidcController was deleted

---
+++ OnlineMarketingTools/Server/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMarketingTools.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMarketingTools.Client.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+	{
+		private readonly IPersonIntegratedRepository _repository;
+
+		public PersonController(IPersonIntegratedRepository personIntegratedRepository)
+		{
+			_repository = personIntegratedRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			var persons = await _repository.GetAll();
+			if (persons.Count() is not 0)
+				return Ok(persons);
+			else
+				return NoContent();
+		}
+	}
+}
