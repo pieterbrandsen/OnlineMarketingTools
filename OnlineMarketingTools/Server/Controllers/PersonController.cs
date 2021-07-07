8a9695533579b9c481ed5aabb61bff6b2ee75832
Jonathan Meibergen <jonathanmeibergen@icloud.com>
2021-07-07 15:06:30 +0200

    UserToken now working

--- OnlineMarketingTools/Server/Controllers/PersonController.cs
+++ OnlineMarketingTools/Server/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using OnlineMarketingTools.Core.Entities;
 using OnlineMarketingTools.Core.Interfaces;
 using System;
@@ -10,6 +11,7 @@ namespace OnlineMarketingTools.Client.Controllers
 {
 	[Route("api/[controller]")]
 	[ApiController]
+	[Authorize]
 	public class PersonController : ControllerBase
 	{
 		private readonly IPersonIntegratedRepository _repository;
